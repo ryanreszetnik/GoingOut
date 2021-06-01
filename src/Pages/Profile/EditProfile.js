d4d7f905b4789493cc68b6c91a86c210d8eaf749
eiliadast <eiliadast@gmail.com>
2021-06-01 11:48:39 -0400

    last night changes

--- src/Pages/Profile/EditProfile.js
+++ src/Pages/Profile/EditProfile.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import { View, Text,Button } from 'react-native'
-import { Auth } from 'aws-amplify';
-import { SET_AUTH_STATUS } from '../../Actions/authActions';
-import { LOGGED_OUT } from '../../Constants/authConstants';
+import React from "react"
+import { View, Text, Button } from "react-native"
+import { Auth } from "aws-amplify"
+import { SET_AUTH_STATUS } from "../../Actions/authActions"
+import { LOGGED_OUT } from "../../Constants/authConstants"
+import { useDispatch } from "react-redux"
 
-export default function EditProfile({navigation}) {
-    async function signOut() {
+export default function EditProfile({ navigation }) {
+  const dispatch = useDispatch()
+  async function signOut() {
     try {
-      await Auth.signOut();
-      dispatch({type:SET_AUTH_STATUS, payload:LOGGED_OUT});
+      await Auth.signOut()
+      dispatch({ type: SET_AUTH_STATUS, payload: LOGGED_OUT })
     } catch (error) {
-      console.log('Error signing out: ', error);
+      console.log("Error signing out: ", error)
     }
   }
-    return (
-        <View>
-            <Text>Edit Profile</Text>
-            <Button title="Back To Profile" color="tomato" onPress={()=>navigation.navigate('ViewProfile')} />
-             <Button title="Sign Out" color="tomato" onPress={signOut} />
-        </View>
-    )
+  return (
+    <View>
+      <Text>Edit Profile</Text>
+      <Button
+        title='Back To Profile'
+        color='tomato'
+        onPress={() => navigation.navigate("ViewProfile")}
+      />
+      <Button title='Sign Out' color='tomato' onPress={signOut} />
+    </View>
+  )
 }
