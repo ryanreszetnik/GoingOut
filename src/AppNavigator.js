62e86168688acb56dfe94089c1d31ba8cb068f9d
eiliadast <eiliadast@gmail.com>
2021-05-31 23:52:48 -0400

    test commit

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react"
+import { View, Text } from "react-native"
 
 export default function AppNavigator() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>hello ryan</Text>
+    </View>
+  )
 }
