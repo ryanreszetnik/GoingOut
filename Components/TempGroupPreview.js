e17939c4b58c0f43d7d05b73f6d70bec2de4ff94
eiliadast <eiliadast@gmail.com>
2021-07-08 13:52:59 -0400

    matches live

--- Components/TempGroupPreview.js
+++ Components/TempGroupPreview.js
@@ -2,30 +2,23 @@ import React from "react"
 import { Text, StyleSheet, View } from "react-native"
 import { TouchableOpacity } from "react-native-gesture-handler"
 import { useDispatch, useSelector } from "react-redux"
-import { SET_CUR_TEMP_GROUP } from "../src/Actions/groupActions"
 
 export default function TempGroupPreview({ group, onPress, id }) {
-  const dispatch = useDispatch()
-  const onSelect = () => {
-    dispatch({ type: SET_CUR_TEMP_GROUP, payload: id })
-    onPress()
-  }
   const baseGroups = group.baseGroups.map((id) =>
     useSelector((state) => state.groups.permGroups).find(
       (group) => group.groupId === id
     )
   )
   return (
-    <TouchableOpacity onPress={onSelect} style={styles.container}>
+    <TouchableOpacity onPress={onPress} style={styles.container}>
       <Text style={styles.header}>{group.name}</Text>
       <Text>Date: {group.date}</Text>
       <Text>Time: {group.time}</Text>
       <Text>
         {`Groups:  `}
-        {baseGroups &&
-          baseGroups.map((group) => {
-            return <Text key={group.groupId}>{group.name}</Text>
-          })}
+        {baseGroups.map((group) => {
+          return <Text key={group.groupId}>{group.name}</Text>
+        })}
       </Text>
     </TouchableOpacity>
   )
