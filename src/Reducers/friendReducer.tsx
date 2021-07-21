a3e9f363c078a47369689492b18c472d4db4d3c1
ryanreszetnik <rres@rogers.com>
2021-07-20 20:54:26 -0400

    fix friends

--- src/Reducers/friendReducer.tsx
+++ src/Reducers/friendReducer.tsx
@@ -1,17 +1,20 @@
 const INITIAL_STATE = []
-import {ADD_FRIEND, REMOVE_FRIEND, SET_FRIENDS} from "../Actions/friendActions"
-import { CONFIRMED, REQUESTED } from "../Constants/friendConstants";
+import {
+  ADD_FRIEND,
+  REMOVE_FRIEND,
+  SET_FRIENDS,
+} from "../Actions/friendActions"
+import { CONFIRMED, REQUESTED } from "../Constants/friendConstants"
 
-export default function friendReducer(state=INITIAL_STATE, action){
-    
-    switch(action.type){
-        case SET_FRIENDS:
-            return [...action.payload]
-        case ADD_FRIEND:
-            return  [...state, action.payload]
-        case REMOVE_FRIEND:
-            return state.filter(f=>f!==action.payload)
-        default:
-            return state;
-    }
+export default function friendReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case SET_FRIENDS:
+      return [...action.payload]
+    case ADD_FRIEND:
+      return [...state, action.payload]
+    case REMOVE_FRIEND:
+      return state.filter((f) => f.sub !== action.payload.sub)
+    default:
+      return state
+  }
 }
